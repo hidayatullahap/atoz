abd5f84e5a6a7ea4998e5540349ba20783ad303b
agung <hidayatullahap@gmail.com>
2018-10-31 11:11:44 +0700

    User namagement

---
+++ plugins/atoz/user/Plugin.php
@@ -0,0 +1,80 @@
+<?php namespace Atoz\User;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * user Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    public $require = ['RainLab.User'];
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'user',
+            'description' => 'No description provided yet...',
+            'author'      => 'tristar',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+
+        return [
+            'Atoz\User\Components\Account' => 'atozAccount',
+        ];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+    }
+}
---
+++ plugins/atoz/user/components/Account.php
@@ -0,0 +1,223 @@
+<?php namespace Atoz\User\Components;
+
+use Cms\Classes\ComponentBase;
+use Auth;
+use Validator;
+use RainLab\User\Models\Settings as UserSettings;
+use Tristar\Fleet\Models\Order as MyOrder;
+use Tristar\Fleet\Models\Cart;
+use Tristar\Fleet\Models\ItemOrder;
+use Session;
+use Cms\Classes\Page;
+use Redirect;
+use Event;
+use ValidationException;
+use Flash;
+
+class Account extends ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'Atoz Account Component',
+            'description' => 'Manage Account'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [
+            'redirect' => [
+                'title'       => /*Redirect to*/'rainlab.user::lang.account.redirect_to',
+                'description' => /*Page name to redirect to after update, sign in or registration.*/'rainlab.user::lang.account.redirect_to_desc',
+                'type'        => 'dropdown',
+                'default'     => ''
+            ]
+        ];
+    }
+
+    public function init()
+    {
+        if (! Auth::check()) return;
+    }
+
+
+    public function getRedirectOptions()
+    {
+        return [''=>'- refresh page -', '0' => '- no redirect -'] + Page::sortBy('baseFileName')->lists('baseFileName', 'baseFileName');
+    }
+
+
+    /**
+     * Returns the login model attribute.
+     */
+    public function loginAttribute()
+    {
+        return UserSettings::get('login_attribute', UserSettings::LOGIN_EMAIL);
+    }
+
+    public function onRun()
+    {
+        $this->page['user'] = $this->user();
+    }
+    
+    public function user()
+    {
+        if (!Auth::check()) {
+            return null;
+        }
+
+        return Auth::getUser();
+    }
+    
+    /**
+     * Sign in the user
+     */
+    public function onSignin()
+    {
+        try {
+            /*
+             * Validate input
+             */
+            $data = post();
+            $rules = [];
+
+            $rules['login'] = 'required|email|between:6,255';
+
+            $rules['password'] = 'required|between:4,255';
+
+            if (!array_key_exists('login', $data)) {
+                $data['login'] = post('username', post('email'));
+            }
+
+            $validation = Validator::make($data, $rules);
+            if ($validation->fails()) {
+                throw new ValidationException($validation);
+            }
+
+            /*
+             * Authenticate user
+             */
+            $credentials = [
+                'login'    => array_get($data, 'login'),
+                'password' => array_get($data, 'password')
+            ];
+
+            Event::fire('rainlab.user.beforeAuthenticate', [$this, $credentials]);
+
+            $user = Auth::authenticate($credentials, true);
+            if ($user->isBanned()) {
+                Auth::logout();
+                throw new AuthException(/*Sorry, this user is currently not activated. Please contact us for further assistance.*/'rainlab.user::lang.account.banned');
+            }
+
+            /*
+             * Redirect
+             */
+            if ($redirect = $this->makeRedirection(true)) {
+                return $redirect;
+            }
+        }
+        catch (Exception $ex) {
+            if (Request::ajax()) throw $ex;
+            else Flash::error($ex->getMessage());
+        }
+    }
+
+    /**
+     * Register the user
+     */
+    public function onRegister()
+    {
+        try {
+            /*
+             * Validate input
+             */
+            $data = post();
+
+            if (!array_key_exists('password_confirmation', $data)) {
+                $data['password_confirmation'] = post('password');
+            }
+
+            $rules = [
+                'email'    => 'required|email|between:6,255',
+                'password' => 'required|between:4,255|confirmed'
+            ];
+
+            if ($this->loginAttribute() == UserSettings::LOGIN_USERNAME) {
+                $rules['username'] = 'required|between:2,255';
+            }
+
+            $validation = Validator::make($data, $rules);
+            if ($validation->fails()) {
+                throw new ValidationException($validation);
+            }
+
+            /*
+             * Register user
+             */
+            Event::fire('rainlab.user.beforeRegister', [&$data]);
+
+            $requireActivation = UserSettings::get('require_activation', true);
+            $automaticActivation = UserSettings::get('activate_mode') == UserSettings::ACTIVATE_AUTO;
+            $userActivation = UserSettings::get('activate_mode') == UserSettings::ACTIVATE_USER;
+            $user = Auth::register($data, $automaticActivation);
+
+            Event::fire('rainlab.user.register', [$user, $data]);
+
+            /*
+             * Activation is by the user, send the email
+             */
+            if ($userActivation) {
+                $this->sendActivationEmail($user);
+
+                Flash::success(Lang::get(/*An activation email has been sent to your email address.*/'rainlab.user::lang.account.activation_email_sent'));
+            }
+
+            /*
+             * Automatically activated or not required, log the user in
+             */
+            if ($automaticActivation || !$requireActivation) {
+                Auth::login($user);
+            }
+
+            /*
+             * Redirect to the intended page after successful sign in
+             */
+            $redirectUrl = $this->pageUrl($this->property('redirect'))
+                ?: $this->property('redirect');
+
+            if ($redirectUrl = post('redirect', $redirectUrl)) {
+                return Redirect::intended($redirectUrl);
+            }
+
+        }
+        catch (Exception $ex) {
+            if (Request::ajax()) throw $ex;
+            else Flash::error($ex->getMessage());
+        }
+    }
+
+    /**
+     * Redirect to the intended page after successful update, sign in or registration.
+     * The URL can come from the "redirect" property or the "redirect" postback value.
+     * @return mixed
+     */
+    protected function makeRedirection($intended = false)
+    {
+        $method = $intended ? 'intended' : 'to';
+
+        $property = $this->property('redirect');
+
+        if (strlen($property) && !$property) {
+            return;
+        }
+
+        $redirectUrl = $this->pageUrl($property) ?: $property;
+
+        if ($redirectUrl = post('redirect', $redirectUrl)) {
+            return Redirect::$method($redirectUrl);
+        }
+    }
+}
+
